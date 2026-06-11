ab2ebe9eb18a826221658a59ac8407c22cbe87c5
Lovely <lovelysharmacji@gmail.com>
2026-06-12 04:30:22 +0530

    SignUp Controller Added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
@@ -10,7 +11,7 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json()); //req.body
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
@@ -22,4 +23,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-app.listen(PORT, () => console.log("Server is listening at port: " + PORT));
+app.listen(PORT, () => {
+  console.log("Server is listening at port: " + PORT);
+  connectDB();
+});
